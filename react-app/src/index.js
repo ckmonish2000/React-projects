94c8238380210e33c7dadf980964a6d10e684caf
ckmonish2000 <ckmonish2000@gmail.com>
2020-07-01 21:45:56 +0530

    multi components

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom"
 import "./style.css";
-import Counter from "./components/Counter.js"
-const ele=<Counter/>
+import Counters from "./components/counters.js"
+const ele=<Counters/>
 ReactDOM.render(ele,document.getElementById("root"))
