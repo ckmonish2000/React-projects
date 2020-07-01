b6e8686b0662ac9e9af5158d802d3d242df33641
ckmonish2000 <ckmonish2000@gmail.com>
2020-07-01 20:12:02 +0530

    conditional rendering

--- react-app/src/components/Counter.js
+++ react-app/src/components/Counter.js
@@ -4,36 +4,25 @@ import React,{Component} from "react";
 class Counter extends Component{
 state={
   count:0,
-  tags:["tag1","tag2","tag3"]
-
+  tags:[]
 };
 
-clr(){
-  var clr=(this.state.count===0) ? "#d19c08" :"#323ea8";
-  var stl={
-    background:clr
-  }
-  return stl
+
+renderTags(){
+  if(this.state.tags.length===0)  return <p> no tags </p>;
+  return     <ul>{ this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>
 }
   render(){
 
   return(
       <React.Fragment>
-    <div className="bn">
-    <h6 className="badge"  style={this.clr()}><h1>{this.formatCount()}</h1></h6>
-    <span>  <button className="incr">increment</button></span>
-    </div>
-    <ul>
-{ this.state.tags.map(tag=><li>{tag}</li>)}
-    </ul>
+      {this.state.tags.length===0 && "enter tags"}
+    {this.renderTags()}
       </React.Fragment>
     );
   }
 
-  formatCount(){
-    const {count}=this.state
-    return count === 0 ? "Zero" : count ;
-  }
+
 }
 
 
