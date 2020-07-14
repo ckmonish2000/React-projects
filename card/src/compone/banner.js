f74db6d83b51e662b27d6ec175ca07d1fe5da3c6
ckmonish2000 <ckmonish2000@gmail.com>
2020-07-14 17:58:39 +0530

    style1

--- card/src/compone/banner.js
+++ card/src/compone/banner.js
@@ -8,9 +8,11 @@ export default class Banner extends Component {
         
         return (
             <div className="banner">
-                <img src={this.props.value.img} alt={this.props.value.id}/>
+                <img className="imgB" src={this.props.value.img} alt={this.props.value.id}/>
+        <div className="Divider">
         <h5>{this.props.value.name}</h5>
         <button type="button" onClick={this.calldel}>del</button>
+        </div>
             </div>
         )
     }
