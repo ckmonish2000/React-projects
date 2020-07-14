9f763a4a15ae2fbb868e21cc6e265df2818bbb3e
ckmonish2000 <ckmonish2000@gmail.com>
2020-07-14 17:42:11 +0530

    style

--- card/src/index.js
+++ card/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Contain from "./compone/contain"
-
+import "./main.css"
 ReactDOM.render(<Contain />,document.getElementById("root"));
