5a0a0234e8cabbfa78250032293ba4765bd4ee0f
ckmonish2000 <ckmonish2000@gmail.com>
2020-07-15 17:29:01 +0530

    del function

--- lists/src/components/container.js
+++ lists/src/components/container.js
@@ -12,11 +12,21 @@ export default class Container extends Component {
             this.setState({data:res.data})
         })
     }
+
+
+    rem=(id)=>{
+        var x=[...this.state.data]
+        // console.log(x)
+       var x1=x.filter(e=>{return e.id!==id})
+       console.log(x1)
+       this.setState({data:x1})
+    }
+    
     render() {
         
         return (
             <div>
-                {this.state.data.map(e=>{return <Item value={e}/>})}
+                {this.state.data.map(e=>{return <Item value={e} rem={this.rem}/>})}
             </div>
         )
     }
--- lists/src/components/item.js
+++ lists/src/components/item.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 
 export default class Item extends Component {
+    del=()=>{
+        this.props.rem(this.props.value.id);
+    }
     render() {
+
         return (
             <div className="box">
                 
         <span> {this.props.value.id}. </span> {this.props.value.title}
-                
+        <button onClick={this.del}>Delete</button>
+
+              
 
             </div>
         )
